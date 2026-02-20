824dcd66ff86923d5364ac8d603d3aa10a09a85e
Genesis Melo <genesis.melo4398@hotmail.com>
2026-02-20 19:30:17 -0300

    fix: add build script

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(errorMiddleware);
 app.use("/auth", authRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http:localhost:${PORT}`);
